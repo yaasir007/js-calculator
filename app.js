f7bd72b0994a387dda7383f82e97568d92e3c7bf
yaasir cheekoory <yaasir1997@gmail.com>
2022-06-24 21:12:44 +0400

    completed the app

--- app.js
+++ app.js
@@ -1,1 +1,34 @@
+let calculator = document.querySelector('.calculator');
+let display = document.querySelector('.display');
+let keys = document.querySelectorAll('.keys');
 
+keys.forEach(key => {
+  key.addEventListener('click', (e) => {
+    const key = e.target;
+    const action = key.dataset.action;
+
+    switch (action) {
+      case 'backspace':
+        if (display.textContent) {
+          display.textContent = display.textContent.slice(0, -1);
+        }
+        break;
+
+      case 'clear':
+        display.textContent = '';
+        break;
+
+      case 'equal':
+        try {
+          display.textContent = eval(display.textContent);
+        } catch (error) {
+          display.textContent = 'Error';
+        }
+        break;
+
+      default:
+        display.textContent += key.textContent;
+        break;
+    }
+  })
+});
